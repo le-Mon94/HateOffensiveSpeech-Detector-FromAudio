92fdd879bf80f1e548a2238c4e35b88ad6b473a2
le-Mon94 <141751601+le-Mon94@users.noreply.github.com>
2023-08-10 21:25:37 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import whisper
+import joblib
+import pandas as pd
+
+import numpy as np
+import statistics
+
+from pydub import AudioSegment # Audio Spliter
+from statistics import mode
+
+detection_model = joblib.load("trained_detector_model.joblib")
+whisper_model = whisper.load_model("medium")
+
+label_mapping = {0: "hate", 1: "offensive", 2: "neither"}
+
+def transcribe(audio):
+
+    text = whisper_model.transcribe(audio)
+
+    text_list = []
+
+    text_list.append(text)
+
+    return text_list
+
+def predict(list_to_check):
+    pred_encoded = detection_model.predict(list_to_check)
+    pred_labels = [label_mapping[pred] for pred in pred_encoded]
+
+    result_df = pd.DataFrame({'text': list_to_check, 'predicted_label': pred_labels})
+
+    result_df['class'] = result_df['predicted_label'].map(label_mapping)
+
+    st.write("Predicted Results:")
+    st.dataframe(result_df)
+
+def main():
+    st.title("Hate / Offensive Speech Detector From Audio")
+    st.write("Upload an MP3 audio file and click the button to continue...:")
+
+    uploaded_file = st.file_uploader("Choose an audio file", type=["mp3"])
+
+    if uploaded_file is not None:
+        st.audio(uploaded_file, format="audio/mp3")
+
+    if st.button("Continue"):
+        if uploaded_file is not None:
+
+            result = transcribe(uploaded_file)
+            str.write(result)
+
+            predict(result)
+
+    else:
+        st.warning("Please upload an audio file before clicking the button.")
+
+if __name__ == "__main__":
+    main()
